242621d28cdabc038cede8e66f8bbf2e82dc2416
Konstantinos Rakopoulos <k.rakopoulos@gmail.com>
2021-11-08 15:04:36 +0200

    Add domain.xml/changes to docker compose

--- payara_nginx_compose/start.sh
+++ payara_nginx_compose/start.sh
@@ -3,6 +3,8 @@
 echo "Creating two files for download"
 dd if=/dev/zero of=test1 bs=1 count=0 seek=250M
 dd if=/dev/zero of=test2 bs=1 count=0 seek=400M
+chmod 777 test1
+chmod 777 test2
 ls -ltra test*
 echo "Starting server and proxy containers"
 docker-compose up &
